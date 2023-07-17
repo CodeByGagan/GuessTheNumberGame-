68bb7916149d31852ddaf8a36df19791fed60cc1
Gagan-0 <gagan0kumar00@gmail.com>
2023-07-17 14:24:25 +0530

    make visually appealing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// assigning variables to access document and manupulate document
+
 const start = document.querySelector("#start");
 const form = document.querySelector("form");
 const submit = document.querySelector("#Submit");
@@ -8,39 +10,48 @@ const playAgain = document.querySelector("#restart");
 const body = document.querySelector("body");
 
 let guessLeft = 5;
+
+// adding eventListener to start button
+
 start.addEventListener("click", () => {
     form.style.display = "flex";
     start.style.display = "none";
     const randomNum = parseInt((Math.random() * 50));
 
-    // after submitting
+    // after click on check button below code will be executed
+
     submit.addEventListener("click", (e) => {
         e.preventDefault();
         guessLeft --;
-        guesses.innerHTML = guessLeft;
+        guesses.innerHTML = `<h1>${guessLeft}</h1>`;
         let guessedNum = userNum.value;
         
         if(guessLeft == 0 && guessedNum == randomNum){
             info.innerHTML = "You Won!";
             won();
-
-        }else if(guessLeft == 0){
-            alert(`You lost, the number was ${randomNum}`);
-            location.reload();
         }
-        if(guessedNum.length == 0){
-            info.innerHTML = "Type some number, you are wasting your guesses.";
-        }else if(guessedNum == randomNum){
-            info.innerHTML = "You Won!";
+        else if(guessLeft == 0){
+            info.innerHTML = "You lost, the number was "+ `${randomNum}`;
+            lost();
+        }
+        else if(guessedNum.length == 0 || guessedNum > 50){
+            info.innerHTML = "Oops! <br> try typing some number between 1 and 50. <br> you are wasting your guesses.";
+        }
+        else if(guessedNum == randomNum){
+            info.innerHTML = "Congratulations You Won!";
             won();
-        }else if(guessedNum > randomNum){
-            info.innerHTML = `Wrong, aim for some lower number. <br> Only ${guessLeft} Guesses are left.`;
-        }else if(guessedNum < randomNum){
-            info.innerHTML = `Wrong, aim for some higher number. <br> Only ${guessLeft} Guesses are left.`;
+        }
+        else if(guessedNum > randomNum){
+            info.innerHTML = `Oops! your guess is wrong <br> Aim for some lower number. <br> Only ${guessLeft} Guesses are left.`;
+        }
+        else if(guessedNum < randomNum){
+            info.innerHTML = `Oops! your guess is wrong <br> Aim for some higher number. <br> Only ${guessLeft} Guesses are left.`;
         } 
     });
 });
 
+
+// function for win condition
 won = () => {
     playAgain.style.display = "block";
     userNum.style.display = "none";
@@ -48,6 +59,15 @@ won = () => {
     body.classList.add("youWon");
 }
 
+// function for lost condition
+lost= () => {
+    playAgain.style.display = "block";
+    userNum.style.display = "none";
+    submit.style.display = "none";
+    body.classList.add("youLost");
+}
+
+// after clicking on play again buttom this code will reload page
 playAgain.addEventListener("click", () => {
     location.reload();
-})
+});
